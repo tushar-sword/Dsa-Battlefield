d326b5e45781953d574a538864854a21bbbf7b50
tushar-sword <tushar21kt@gmail.com>
2026-02-08 21:20:27 +0530

    Array medium 12 8Feb26

---
+++ Arrays/Medium_Level/Rotateby90/bruteForce.java
@@ -0,0 +1,38 @@
+package Arrays.Medium_Level.Rotateby90;
+
+public class bruteForce {
+    // to do this 
+    // you get array 
+    // make arr[j][n-i-1] = arr[i][j]
+
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+
+    // arr[0][3-0-1=2]=arr[0][0]
+    //     1
+    // arr[1][3-0-1=2]=arr[0][1]
+    //     1
+    //     2
+    // arr[2][3-0-1=2]=arr[0][2]
+    //     1
+    //     2
+    //     3
+    // arr[0][3-1-1=1]=arr[1][0]
+    //   4 1
+    //     2
+    //     3
+    // arr[1][3-1-1=1]=arr[1][1]
+    //   4 1
+    //   5 2
+    //     3 
+    // arr[2][3-1-1=1]=arr[1][2]
+    //   4 1
+    //   5 2
+    //   6 3     
+    // more 
+    // 7 4 1
+    // 8 5 2
+    // 9 6 3  
+    
+}
